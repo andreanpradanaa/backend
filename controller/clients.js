40123728eed275157d1459927f04fb4afa84d88c
andreanpradanaa <andreanpradana2001@gmail.com>
2021-11-02 03:50:00 +0700

    commit

--- controller/clients.js
+++ controller/clients.js
@@ -2,18 +2,40 @@ const express = require("express");
 const multer = require("multer");
 const router = express.Router();
 const Clients = require("../model/clients");
+const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "./uploads/clients ");
-  },
-
-  filename: (req, file, callback) => {
-    callback(null, file.originalname);
+var storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname);
   },
 });
+var imageFilter = function (req, file, cb) {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+var uploads = multer({ storage: storage, fileFilter: imageFilter });
 
-const uploads = multer({ storage: storage });
+var cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: "apaaja2001",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, "./uploads/clients ");
+//   },
+
+//   filename: (req, file, callback) => {
+//     callback(null, file.originalname);
+//   },
+// });
+
+// const uploads = multer({ storage: storage });
 
 router.get("/", (req, res) => {
   Clients.find()
@@ -22,15 +44,33 @@ router.get("/", (req, res) => {
 });
 
 router.post("/add", uploads.single("gambar"), (req, res) => {
-  const newClient = new Clients({
-    nama: req.body.nama,
-    gambar: req.file.originalname,
+  cloudinary.uploader.upload(req.file.path, function (result) {
+    if (err) {
+      req.flash("error", err.message);
+      return res.redirect("back");
+    }
+    // add cloudinary url for the image to the campground object under image property
+    req.body.client.gambar = result.secure_url;
+    req.body.client.nama = req.user.nama;
+    // add author to client
+
+    Client.create(req.body.client, function (err, client) {
+      if (err) {
+        req.flash("error", err.message);
+        return res.redirect("back");
+      }
+      res.redirect("/client/" + client.id);
+    });
   });
 
-  newClient
-    .save()
-    .then(() => res.json("added succes!"))
-    .catch((err) => res.status(400).json(`Error: ${err}`));
+  // const newClient = new Clients({
+  //   nama: req.body.nama,
+  //   gambar: req.file.originalname,
+  // });
+  // newClient
+  //   .save()
+  //   .then(() => res.json("added succes!"))
+  //   .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 router.get("/:id", (req, res) => {
--- controller/login.js
+++ controller/login.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const User = require("../model/user");
 const jwt = require("jsonwebtoken");
 
